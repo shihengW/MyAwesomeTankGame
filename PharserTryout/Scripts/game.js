7a45283c280d307073cae5cfb7bcc9aaacf047dc
shihengW <shiheng.wei@hotmail.com>
2017-04-23 16:35:45 +0800

    Just create the enviorment.

---
+++ PharserTryout/Scripts/game.js
@@ -0,0 +1,21 @@
+var SimpleGame = (function () {
+    function SimpleGame() {
+        this.game = new Phaser.Game(800, 600, Phaser.AUTO, 'content', { create: this.create });
+    }
+    SimpleGame.prototype.create = function () {
+        var text = "Hello World!";
+        var style = { font: "65px Arial", fill: "#ff0000", align: "center" };
+        this.game.add.text(0, 0, text, style);
+    };
+    return SimpleGame;
+}());
+window.onload = function () {
+    var game = new SimpleGame();
+};
+var Tank = (function () {
+    function Tank() {
+    }
+    Tank.prototype.Fire = function () {
+    };
+    return Tank;
+}());
