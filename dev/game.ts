31191495383f17b16568d8cd229c0a00ebbe30c3
shihengW <shiheng.wei@hotmail.com>
2017-04-27 19:07:30 +0800

    reorganized the file system

--- dev/game.ts
+++ dev/game.ts
@@ -1,25 +1,20 @@
-﻿// Road map:
-// [Check] 1. Collision, physics and move
-//   1.1. Sand bag
-//   1.2. X direction
-// 2. Animation and realistic movement
-//   2.1. Sprite sheet.
-//   2.2. Accelerate when key down, and slow down when key up.
-// [Working] 3. Gun towner controlled by mouse
-// ----------------
-// 4. Multi-users
-
-window.onload = () => {
-    var game = new SimpleGame();
-};
-
-enum Directions { Up, Down, Left, Right, None }
-
+﻿/// ****** names and parameters. ****** ///
+// Names
 const sandbagName: string = "sandbag";
 const tankbodyName: string = "tankbody";
 const guntowerName: string = "guntower";
 const bulletName: string = "bullet";
 const particleName: string = "particle";
+// Parameters
+const playerSpeed: number = 10;
+/// ********************************************************** ///
+
+/// *** Game main class *** ///
+window.onload = () => {
+    var game = new SimpleGame();
+};
+
+enum Directions { Up, Down, Left, Right, None }
 
 class SimpleGame {
     game: Phaser.Game;
@@ -131,164 +126,6 @@ class SimpleGame {
     }
 }
 
-class Tank {
-    private tank: Phaser.Group;
-    private guntower: Phaser.Sprite;
-    private tankbody: Phaser.Sprite;
-
-    private bullets: Phaser.Group;
-    private ownerGame: Phaser.Game;
-    private tankSpeed: number = 3;
-    private direction: Directions = Directions.None;
-
-    constructor(game: Phaser.Game) {
-        this.ownerGame = game;
-
-        // Creat tank.
-        this.tank = game.add.group(game, "tank", true, true, Phaser.Physics.ARCADE);
-        this.tank.position.set(game.width / 2, game.height / 2);
-
-        // Seperate tank body and gun tower.           
-        this.tankbody = this.tank.create(0, 0, tankbodyName);
-        this.guntower = this.tank.create(0, 0, guntowerName);
-        this.tank.setAll("anchor", new Phaser.Point(0.5, 0.5));
-
-        // NOT Only enable the physics for tankbody.
-        // this.ownerGame.physics.arcade.enable(this.tankbody);
-        this.tankbody.body.collideWorldBounds = true;
-        this.tankbody.body.bounce.y = 1;
-        this.tankbody.body.bounce.x = 1;
-        this.tankbody.body.mass = 70;
-        
-        // Create bullets.
-        this.bullets = game.add.group();
-        this.bullets.enableBody = true;
-        this.bullets.physicsBodyType = Phaser.Physics.ARCADE;
-        this.bullets.createMultiple(30, bulletName);
-
-        this.bullets.setAll("checkWorldBounds", true);
-        this.bullets.setAll("outOfBoundsKill", true);
-    }
-
-    tankStartMove(d: Directions) {
-        this.direction = d;
-        switch (d) {
-            case Directions.Up:
-                this.tankbody.angle = 0;
-                return;
-            case Directions.Left:
-                this.tankbody.angle = -90;
-                return;
-            case Directions.Down:
-                this.tankbody.angle = 180;
-                return;
-            case Directions.Right:
-                this.tankbody.angle = 90;
-                return;
-        }
-    }
-
-    getDirection() : Directions {
-        return this.direction;
-    }
-
-    tankEndMove() {
-        this.direction = Directions.None;
-    }
-
-    tankUpdate() {
-        // First, move gun tower to point to mouse.
-        const angle: number = Phaser.Math.angleBetweenPoints(this.ownerGame.input.activePointer.position, this.tankbody.body.position);
-        this.guntower.angle = Phaser.Math.radToDeg(angle) - 90;
-
-        // Second, move the tank.
-        // TODO: Find a better way to move the tank.
-        if (this.direction === Directions.None) {
-            this.tankbody.body.velocity.x = 0;
-            this.tankbody.body.velocity.y = 0;
-        }
-
-        switch (this.direction) {
-            case Directions.None:
-                return;
-            case Directions.Up:
-                this.tank.position.add(0, -1 * this.tankSpeed);
-                return;
-            case Directions.Down:
-                this.tank.position.add(0, this.tankSpeed);
-                return;
-            case Directions.Left:
-                this.tank.position.add(-1 * this.tankSpeed, 0);
-                return;
-            case Directions.Right:
-                this.tank.position.add(this.tankSpeed, 0);
-                return;
-            default:
-        }
-
-        // Finally, force to coordinate the guntower and tankbody.
-        // TODO: Find a smarter way to do this.
-        this.guntower.body.position = this.tankbody.body.position;        
-    }
-
-    private nextFire: number = 0;
-    private fireRate: number = 200;
-
-    tankFire() {
-        if (this.ownerGame.time.now < this.nextFire || this.bullets.countDead() <= 0) {
-            return;
-        }
-
-        // Set the cooldown time.
-        this.nextFire = this.ownerGame.time.now + this.fireRate;
-
-        // Get a random offset.
-        const randomAngleOffset: number = (Math.random() - 0.5) * 0;
-        const theta: number = Phaser.Math.degToRad(this.guntower.angle) + randomAngleOffset;
-
-        // Bullet start position offset.
-        const halfLength: number = this.guntower.height / 2;
-        let xOffset: number = Math.sin(theta) * halfLength;
-        let yOffset: number = -1 * Math.cos(theta) * halfLength;
-
-        // Get bullet.
-        const bullet: Phaser.Sprite = this.bullets.getFirstDead();
-        bullet.anchor.set(0.5, 0.5);
-        bullet.angle = this.guntower.angle;
-
-        // Get the position of the tank.
-        let guntowerPosition = this.tankbody.body.center;
-
-        bullet.reset(guntowerPosition.x + xOffset, guntowerPosition.y + yOffset);
-
-        const longway: number = 10000;
-        xOffset = Math.sin(theta) * longway;
-        yOffset = -1 * Math.cos(theta) * longway;
-        this.ownerGame.physics.arcade.moveToXY(bullet, 
-            guntowerPosition.x + xOffset, guntowerPosition.y + yOffset, 1000);
-    }
-
-    checkCollide(another: Phaser.Sprite) {
-        let self = this;
-
-        this.ownerGame.physics.arcade.collide(this.tankbody, another);
-        this.bullets.forEachAlive((item:Phaser.Sprite) => {
-            self.ownerGame.physics.arcade.collide(item, another, 
-                (bullet: Phaser.Sprite, another: Phaser.Sprite) => Tank.BulletHit(bullet, another, self.ownerGame));
-        }, this);
-    }
-
-    static BulletHit(bullet: Phaser.Sprite, another: Phaser.Sprite, game: Phaser.Game) {
-        bullet.kill();
-        // Now we are creating the particle emitter, centered to the world
-        let emitter = game.add.emitter((bullet.x + another.x) / 2, (bullet.y + another.y) / 2);
-        emitter.makeParticles(particleName, 0, 50, false, false);
-        emitter.explode(300, 50);
-    }
-}
-
-const playerSpeed: number = 10;
-
 // Don't touch anything else, just refactor this class first.
 class PhysicsManager {
 
@@ -387,3 +224,153 @@ class PhysicsManager {
         return angle > 0 && angle < 180;
     }
 }
+
+/// ********************************************************** ///
+
+/// *** tank class *** ///
+class Tank {
+    private tank: Phaser.Group;
+    private guntower: Phaser.Sprite;
+    private tankbody: Phaser.Sprite;
+
+    private bullets: Phaser.Group;
+    private ownerGame: Phaser.Game;
+    private tankSpeed: number = 3;
+    private direction: Directions = Directions.None;
+
+    constructor(game: Phaser.Game) {
+        this.ownerGame = game;
+
+        // Creat tank.
+        this.tank = game.add.group(game, "tank", true, true, Phaser.Physics.ARCADE);
+        this.tank.position.set(game.width / 2, game.height / 2);
+
+        // Seperate tank body and gun tower.           
+        this.tankbody = this.tank.create(0, 0, tankbodyName);
+        this.guntower = this.tank.create(0, 0, guntowerName);
+        this.tank.setAll("anchor", new Phaser.Point(0.5, 0.5));
+
+        // NOT Only enable the physics for tankbody.
+        // this.ownerGame.physics.arcade.enable(this.tankbody);
+        this.tankbody.body.collideWorldBounds = true;
+        this.tankbody.body.bounce.y = 1;
+        this.tankbody.body.bounce.x = 1;
+        this.tankbody.body.mass = 70;
+        
+        // Create bullets.
+        this.bullets = game.add.group();
+        this.bullets.enableBody = true;
+        this.bullets.physicsBodyType = Phaser.Physics.ARCADE;
+        this.bullets.createMultiple(30, bulletName);
+
+        this.bullets.setAll("checkWorldBounds", true);
+        this.bullets.setAll("outOfBoundsKill", true);
+    }
+
+    tankStartMove(d: Directions) {
+        this.direction = d;
+        switch (d) {
+            case Directions.Up:
+                this.tankbody.angle = 0;
+                return;
+            case Directions.Left:
+                this.tankbody.angle = -90;
+                return;
+            case Directions.Down:
+                this.tankbody.angle = 180;
+                return;
+            case Directions.Right:
+                this.tankbody.angle = 90;
+                return;
+        }
+    }
+
+    getDirection() : Directions {
+        return this.direction;
+    }
+
+    tankEndMove() {
+        this.direction = Directions.None;
+    }
+
+    tankUpdate() {
+        // First, move gun tower to point to mouse.
+        const angle: number = Phaser.Math.angleBetweenPoints(this.ownerGame.input.activePointer.position, this.tankbody.body.position);
+        this.guntower.angle = Phaser.Math.radToDeg(angle) - 90;
+
+        // Second, move the tank.
+        // TODO: Find a better way to move the tank.
+        if (this.direction === Directions.None) {
+            this.tankbody.body.velocity.x = 0;
+            this.tankbody.body.velocity.y = 0;
+        }
+        switch (this.direction) {
+            case Directions.None: break;
+            case Directions.Up: this.tank.position.add(0, -1 * this.tankSpeed); break;
+            case Directions.Down: this.tank.position.add(0, this.tankSpeed); break;
+            case Directions.Left: this.tank.position.add(-1 * this.tankSpeed, 0); break;
+            case Directions.Right: this.tank.position.add(this.tankSpeed, 0); break;
+            default: break;
+        }
+
+        // Finally, force to coordinate the guntower and tankbody.
+        // TODO: Find a smarter way to do this.
+        this.guntower.position = this.tankbody.position;        
+    }
+
+    private nextFire: number = 0;
+    private fireRate: number = 200;
+
+    tankFire() {
+        if (this.ownerGame.time.now < this.nextFire || this.bullets.countDead() <= 0) {
+            return;
+        }
+
+        // Set the cooldown time.
+        this.nextFire = this.ownerGame.time.now + this.fireRate;
+
+        // Get a random offset.
+        const randomAngleOffset: number = (Math.random() - 0.5) * 0;
+        const theta: number = Phaser.Math.degToRad(this.guntower.angle) + randomAngleOffset;
+
+        // Bullet start position offset.
+        const halfLength: number = this.guntower.height / 2;
+        let xOffset: number = Math.sin(theta) * halfLength;
+        let yOffset: number = -1 * Math.cos(theta) * halfLength;
+
+        // Get bullet.
+        const bullet: Phaser.Sprite = this.bullets.getFirstDead();
+        bullet.anchor.set(0.5, 0.5);
+        bullet.angle = this.guntower.angle;
+
+        // Get the position of the tank.
+        let guntowerPosition = this.tankbody.body.center;
+
+        bullet.reset(guntowerPosition.x + xOffset, guntowerPosition.y + yOffset);
+
+        const longway: number = 10000;
+        xOffset = Math.sin(theta) * longway;
+        yOffset = -1 * Math.cos(theta) * longway;
+        this.ownerGame.physics.arcade.moveToXY(bullet, 
+            guntowerPosition.x + xOffset, guntowerPosition.y + yOffset, 1000);
+    }
+
+    checkCollide(another: Phaser.Sprite) {
+        let self = this;
+
+        this.ownerGame.physics.arcade.collide(this.tankbody, another);
+        this.bullets.forEachAlive((item:Phaser.Sprite) => {
+            self.ownerGame.physics.arcade.collide(item, another, 
+                (bullet: Phaser.Sprite, another: Phaser.Sprite) => Tank.BulletHit(bullet, another, self.ownerGame));
+        }, this);
+    }
+
+    static BulletHit(bullet: Phaser.Sprite, another: Phaser.Sprite, game: Phaser.Game) {
+        bullet.kill();
+        // Now we are creating the particle emitter, centered to the world
+        let emitter = game.add.emitter((bullet.x + another.x) / 2, (bullet.y + another.y) / 2);
+        emitter.makeParticles(particleName, 0, 50, false, false);
+        emitter.explode(300, 50);
+    }
+}
+/// ********************************************************** ///
--- scripts/game.js
+++ scripts/game.js
@@ -1,13 +1,14 @@
-// Road map:
-// [Check] 1. Collision, physics and move
-//   1.1. Sand bag
-//   1.2. X direction
-// 2. Animation and realistic movement
-//   2.1. Sprite sheet.
-//   2.2. Accelerate when key down, and slow down when key up.
-// [Working] 3. Gun towner controlled by mouse
-// ----------------
-// 4. Multi-users
+/// ****** names and parameters. ****** ///
+// Names
+var sandbagName = "sandbag";
+var tankbodyName = "tankbody";
+var guntowerName = "guntower";
+var bulletName = "bullet";
+var particleName = "particle";
+// Parameters
+var playerSpeed = 10;
+/// ********************************************************** ///
+/// *** Game main class *** ///
 window.onload = function () {
     var game = new SimpleGame();
 };
@@ -19,11 +20,6 @@ var Directions;
     Directions[Directions["Right"] = 3] = "Right";
     Directions[Directions["None"] = 4] = "None";
 })(Directions || (Directions = {}));
-var sandbagName = "sandbag";
-var tankbodyName = "tankbody";
-var guntowerName = "guntower";
-var bulletName = "bullet";
-var particleName = "particle";
 var SimpleGame = (function () {
     function SimpleGame() {
         this.game = new Phaser.Game(800, 600, Phaser.AUTO, 'content', {
@@ -122,130 +118,6 @@ var SimpleGame = (function () {
     };
     return SimpleGame;
 }());
-var Tank = (function () {
-    function Tank(game) {
-        this.tankSpeed = 3;
-        this.direction = Directions.None;
-        this.nextFire = 0;
-        this.fireRate = 200;
-        this.ownerGame = game;
-        // Creat tank.
-        this.tank = game.add.group(game, "tank", true, true, Phaser.Physics.ARCADE);
-        this.tank.position.set(game.width / 2, game.height / 2);
-        // Seperate tank body and gun tower.           
-        this.tankbody = this.tank.create(0, 0, tankbodyName);
-        this.guntower = this.tank.create(0, 0, guntowerName);
-        this.tank.setAll("anchor", new Phaser.Point(0.5, 0.5));
-        // NOT Only enable the physics for tankbody.
-        // this.ownerGame.physics.arcade.enable(this.tankbody);
-        this.tankbody.body.collideWorldBounds = true;
-        this.tankbody.body.bounce.y = 1;
-        this.tankbody.body.bounce.x = 1;
-        this.tankbody.body.mass = 70;
-        // Create bullets.
-        this.bullets = game.add.group();
-        this.bullets.enableBody = true;
-        this.bullets.physicsBodyType = Phaser.Physics.ARCADE;
-        this.bullets.createMultiple(30, bulletName);
-        this.bullets.setAll("checkWorldBounds", true);
-        this.bullets.setAll("outOfBoundsKill", true);
-    }
-    Tank.prototype.tankStartMove = function (d) {
-        this.direction = d;
-        switch (d) {
-            case Directions.Up:
-                this.tankbody.angle = 0;
-                return;
-            case Directions.Left:
-                this.tankbody.angle = -90;
-                return;
-            case Directions.Down:
-                this.tankbody.angle = 180;
-                return;
-            case Directions.Right:
-                this.tankbody.angle = 90;
-                return;
-        }
-    };
-    Tank.prototype.getDirection = function () {
-        return this.direction;
-    };
-    Tank.prototype.tankEndMove = function () {
-        this.direction = Directions.None;
-    };
-    Tank.prototype.tankUpdate = function () {
-        // First, move gun tower to point to mouse.
-        var angle = Phaser.Math.angleBetweenPoints(this.ownerGame.input.activePointer.position, this.tankbody.body.position);
-        this.guntower.angle = Phaser.Math.radToDeg(angle) - 90;
-        // Second, move the tank.
-        // TODO: Find a better way to move the tank.
-        if (this.direction === Directions.None) {
-            this.tankbody.body.velocity.x = 0;
-            this.tankbody.body.velocity.y = 0;
-        }
-        switch (this.direction) {
-            case Directions.None:
-                return;
-            case Directions.Up:
-                this.tank.position.add(0, -1 * this.tankSpeed);
-                return;
-            case Directions.Down:
-                this.tank.position.add(0, this.tankSpeed);
-                return;
-            case Directions.Left:
-                this.tank.position.add(-1 * this.tankSpeed, 0);
-                return;
-            case Directions.Right:
-                this.tank.position.add(this.tankSpeed, 0);
-                return;
-            default:
-        }
-        // Finally, force to coordinate the guntower and tankbody.
-        // TODO: Find a smarter way to do this.
-        this.guntower.body.position = this.tankbody.body.position;
-    };
-    Tank.prototype.tankFire = function () {
-        if (this.ownerGame.time.now < this.nextFire || this.bullets.countDead() <= 0) {
-            return;
-        }
-        // Set the cooldown time.
-        this.nextFire = this.ownerGame.time.now + this.fireRate;
-        // Get a random offset.
-        var randomAngleOffset = (Math.random() - 0.5) * 0;
-        var theta = Phaser.Math.degToRad(this.guntower.angle) + randomAngleOffset;
-        // Bullet start position offset.
-        var halfLength = this.guntower.height / 2;
-        var xOffset = Math.sin(theta) * halfLength;
-        var yOffset = -1 * Math.cos(theta) * halfLength;
-        // Get bullet.
-        var bullet = this.bullets.getFirstDead();
-        bullet.anchor.set(0.5, 0.5);
-        bullet.angle = this.guntower.angle;
-        // Get the position of the tank.
-        var guntowerPosition = this.tankbody.body.center;
-        bullet.reset(guntowerPosition.x + xOffset, guntowerPosition.y + yOffset);
-        var longway = 10000;
-        xOffset = Math.sin(theta) * longway;
-        yOffset = -1 * Math.cos(theta) * longway;
-        this.ownerGame.physics.arcade.moveToXY(bullet, guntowerPosition.x + xOffset, guntowerPosition.y + yOffset, 1000);
-    };
-    Tank.prototype.checkCollide = function (another) {
-        var self = this;
-        this.ownerGame.physics.arcade.collide(this.tankbody, another);
-        this.bullets.forEachAlive(function (item) {
-            self.ownerGame.physics.arcade.collide(item, another, function (bullet, another) { return Tank.BulletHit(bullet, another, self.ownerGame); });
-        }, this);
-    };
-    Tank.BulletHit = function (bullet, another, game) {
-        bullet.kill();
-        // Now we are creating the particle emitter, centered to the world
-        var emitter = game.add.emitter((bullet.x + another.x) / 2, (bullet.y + another.y) / 2);
-        emitter.makeParticles(particleName, 0, 50, false, false);
-        emitter.explode(300, 50);
-    };
-    return Tank;
-}());
-var playerSpeed = 10;
 // Don't touch anything else, just refactor this class first.
 var PhysicsManager = (function () {
     function PhysicsManager() {
@@ -342,4 +214,129 @@ var PhysicsManager = (function () {
     };
     return PhysicsManager;
 }());
+/// ********************************************************** ///
+/// *** tank class *** ///
+var Tank = (function () {
+    function Tank(game) {
+        this.tankSpeed = 3;
+        this.direction = Directions.None;
+        this.nextFire = 0;
+        this.fireRate = 200;
+        this.ownerGame = game;
+        // Creat tank.
+        this.tank = game.add.group(game, "tank", true, true, Phaser.Physics.ARCADE);
+        this.tank.position.set(game.width / 2, game.height / 2);
+        // Seperate tank body and gun tower.           
+        this.tankbody = this.tank.create(0, 0, tankbodyName);
+        this.guntower = this.tank.create(0, 0, guntowerName);
+        this.tank.setAll("anchor", new Phaser.Point(0.5, 0.5));
+        // NOT Only enable the physics for tankbody.
+        // this.ownerGame.physics.arcade.enable(this.tankbody);
+        this.tankbody.body.collideWorldBounds = true;
+        this.tankbody.body.bounce.y = 1;
+        this.tankbody.body.bounce.x = 1;
+        this.tankbody.body.mass = 70;
+        // Create bullets.
+        this.bullets = game.add.group();
+        this.bullets.enableBody = true;
+        this.bullets.physicsBodyType = Phaser.Physics.ARCADE;
+        this.bullets.createMultiple(30, bulletName);
+        this.bullets.setAll("checkWorldBounds", true);
+        this.bullets.setAll("outOfBoundsKill", true);
+    }
+    Tank.prototype.tankStartMove = function (d) {
+        this.direction = d;
+        switch (d) {
+            case Directions.Up:
+                this.tankbody.angle = 0;
+                return;
+            case Directions.Left:
+                this.tankbody.angle = -90;
+                return;
+            case Directions.Down:
+                this.tankbody.angle = 180;
+                return;
+            case Directions.Right:
+                this.tankbody.angle = 90;
+                return;
+        }
+    };
+    Tank.prototype.getDirection = function () {
+        return this.direction;
+    };
+    Tank.prototype.tankEndMove = function () {
+        this.direction = Directions.None;
+    };
+    Tank.prototype.tankUpdate = function () {
+        // First, move gun tower to point to mouse.
+        var angle = Phaser.Math.angleBetweenPoints(this.ownerGame.input.activePointer.position, this.tankbody.body.position);
+        this.guntower.angle = Phaser.Math.radToDeg(angle) - 90;
+        // Second, move the tank.
+        // TODO: Find a better way to move the tank.
+        if (this.direction === Directions.None) {
+            this.tankbody.body.velocity.x = 0;
+            this.tankbody.body.velocity.y = 0;
+        }
+        switch (this.direction) {
+            case Directions.None: break;
+            case Directions.Up:
+                this.tank.position.add(0, -1 * this.tankSpeed);
+                break;
+            case Directions.Down:
+                this.tank.position.add(0, this.tankSpeed);
+                break;
+            case Directions.Left:
+                this.tank.position.add(-1 * this.tankSpeed, 0);
+                break;
+            case Directions.Right:
+                this.tank.position.add(this.tankSpeed, 0);
+                break;
+            default: break;
+        }
+        // Finally, force to coordinate the guntower and tankbody.
+        // TODO: Find a smarter way to do this.
+        this.guntower.position = this.tankbody.position;
+    };
+    Tank.prototype.tankFire = function () {
+        if (this.ownerGame.time.now < this.nextFire || this.bullets.countDead() <= 0) {
+            return;
+        }
+        // Set the cooldown time.
+        this.nextFire = this.ownerGame.time.now + this.fireRate;
+        // Get a random offset.
+        var randomAngleOffset = (Math.random() - 0.5) * 0;
+        var theta = Phaser.Math.degToRad(this.guntower.angle) + randomAngleOffset;
+        // Bullet start position offset.
+        var halfLength = this.guntower.height / 2;
+        var xOffset = Math.sin(theta) * halfLength;
+        var yOffset = -1 * Math.cos(theta) * halfLength;
+        // Get bullet.
+        var bullet = this.bullets.getFirstDead();
+        bullet.anchor.set(0.5, 0.5);
+        bullet.angle = this.guntower.angle;
+        // Get the position of the tank.
+        var guntowerPosition = this.tankbody.body.center;
+        bullet.reset(guntowerPosition.x + xOffset, guntowerPosition.y + yOffset);
+        var longway = 10000;
+        xOffset = Math.sin(theta) * longway;
+        yOffset = -1 * Math.cos(theta) * longway;
+        this.ownerGame.physics.arcade.moveToXY(bullet, guntowerPosition.x + xOffset, guntowerPosition.y + yOffset, 1000);
+    };
+    Tank.prototype.checkCollide = function (another) {
+        var self = this;
+        this.ownerGame.physics.arcade.collide(this.tankbody, another);
+        this.bullets.forEachAlive(function (item) {
+            self.ownerGame.physics.arcade.collide(item, another, function (bullet, another) { return Tank.BulletHit(bullet, another, self.ownerGame); });
+        }, this);
+    };
+    Tank.BulletHit = function (bullet, another, game) {
+        bullet.kill();
+        // Now we are creating the particle emitter, centered to the world
+        var emitter = game.add.emitter((bullet.x + another.x) / 2, (bullet.y + another.y) / 2);
+        emitter.makeParticles(particleName, 0, 50, false, false);
+        emitter.explode(300, 50);
+    };
+    return Tank;
+}());
+/// ********************************************************** /// 
 //# sourceMappingURL=game.js.map
