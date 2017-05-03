c263ce0d87c1c103f609733506e844ed8fa63366
Shiheng Wei <shiheng.wei@ni.com>
2017-05-03 12:07:17 +0800

    Update the input system.

--- dev/directions.ts
+++ dev/directions.ts
@@ -1,1 +1,11 @@
-enum Directions { Up, Down, Left, Right, None }
+enum Directions { 
+    None,
+    Up = 1 << 1, 
+    Down = 1 << 2, 
+    Left = 1 << 3, 
+    Right = 1 << 4,
+    UpLeft = Up | Left,
+    UpRight = Up | Right,
+    DownLeft = Down | Left,
+    DownRight = Down | Right 
+}
